58034760714d3b4653d806ef1980b132b75d41a0
River Holley <riverholley0923@gmail.com>
2019-08-31 12:22:44 -0500

    Test for optional pull on others machines; See if they can see the log

--- Project/HelloWorld.java
+++ Project/HelloWorld.java
@@ -7,6 +7,7 @@ public class HelloWorld
     public static void main(String[] args) {
         System.out.println("Hello World!");
         //This is the test comment
+        // This is a response to test pull
     }
 }
 
