59d0e2cc6561bb70d440a999d730c879f3dee63a
River Holley <riverholley0923@gmail.com>
2019-09-11 15:56:59 -0500

    Added csv parser

--- Project/HelperFiles/Resources_for_Students/parse_csv.py
+++ Project/HelperFiles/Resources_for_Students/parse_csv.py
@@ -1,5 +1,6 @@
 # parse_csv usitility for java project
 
+import csv
 import pandas as pd
 
 '''
@@ -8,15 +9,15 @@ File purpose: Read in and parse csv (Stars)
 to make a new version of only data we need
 so later on, java file can access each as an object
 '''
+
 def read_data(filename: str):
     '''
     Function will read in a csv 
-    and store it into a pandas datafram
+    and store it into a pandas dataframe
     '''
     df = pd.read_csv(filename)
 
-    df.head()
-
+    return df
 
 
 def main():
@@ -24,7 +25,20 @@ def main():
     Main Function
     '''
     path = '/home/river/Desktop/Senior_Project/Project/HelperFiles/Resources_for_Students/hyg.csv'
-    read_data(path)
+    output = "Needed_Mag.csv"
+    dataFrame = read_data(path) 
+    # here we can select any header type and pull it
+    mag_list = dataFrame['Mag'].values
+    adj_mag_list = []
+
+    for element in mag_list:
+        if element < 6.0 and element > 0.0:
+            adj_mag_list.append(element)
+    
+    writeCSV(output, adj_mag_list)
+
+    
+
 
 
 if __name__ == "__main__":
