fb63030ec335b8e87050a48dc13e7365318d7305
River Holley <denvercoder2@gmail.com>
2019-09-13 22:51:45 -0500

    XML parser now will gather correct magnitude data, star id to match will come next

--- Project/HelperFiles/Resources_for_Students/parse_xml.py
+++ Project/HelperFiles/Resources_for_Students/parse_xml.py
@@ -1,58 +1,35 @@
 # parse_csv usitility for java project
+import xml.etree.ElementTree as etree
 
-import csv
-import pandas as pd
-'''
-Scott Holley
-File purpose: Read in and parse csv (Stars)
-to make a new version of only data we need
-so later on, java file can access each as an object
-'''
-
-def read_data(filename: str):
+def read_xml(xml_file: str):
     '''
-    Function will read in a csv 
-    and store it into a pandas dataframe
+    Function will read and parse an xml file
     '''
-    df = pd.read_csv(filename)
+    tree = etree.parse(xml_file)
+    root = tree.getroot()
+    for row in root:
+        for item in row:
+            if item.tag == "Mag":
+                if float(item.text) < 6.0 and float(item.text) > 0.0:
+                    print(item.tag, ":",  item.text)
 
-    return df
+    # print(StarID[0])
+    # here you can grab them all dependent on for loop
 
-def make_list(dataframe) -> list:
-    '''
-    Function will take dataframe and convert to list
-    '''
-    new_list = []
-    for item in dataframe:
-        new_list.append(item)
-    
-
-    return new_list
+    # pull if within magnitude rating
+    # for rate in Magnitude:
+    #     typed_rate = float(rate.text)
+    #     if typed_rate < 6.0 and typed_rate > 0.0:
+    #         print(typed_rate)
 
 
 def main():
     '''
     Main Function
     '''
-    # sawp between os here: Add a conditional in the future if needed
-    windows = '\\Users\River\Desktop\Senior_Project\Project\HelperFiles\Resources_for_Students\hyg.csv'
-    linux = '/home/river/Desktop/Senior_Project/Project/HelperFiles/Resources_for_Students/hyg.csv'
-    output = "Needed_Mag.csv"
-    dataFrame = read_data(windows) 
-    # here we can select any header type and pull it
-    mag_d_list = make_list(dataFrame['Mag'].values)
-    starId_List = (dataFrame['StarID'].values)
-    li = make_list(starId_List)
-    with open ("Stars_Required.csv", "w") as outfile:
-        writer = csv.writer(outfile)
-        for item in mag_d_list:
-            if item < 6.0 and item > 0.0:
-                find_star = mag_d_list.index(item)
-                adj_list = []
-                adj_list.append(item)
-                writer.writerow(adj_list)
+    path = '\\Users\River\Desktop\Senior_Project\Project\HelperFiles\Resources_for_Students\stars.xml'
+    read_xml(path)
 
 
 if __name__ == "__main__":
-    
     main()
