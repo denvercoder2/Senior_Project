79f9fa057ed689da815a4707e8022cb5aff66078
River Holley <denvercoder2@gmail.com>
2019-09-15 17:03:40 -0500

    Class structure used so can be called from other places and return objects

--- Project/HelperFiles/Resources_for_Students/parse_xml.py
+++ Project/HelperFiles/Resources_for_Students/parse_xml.py
@@ -1,32 +1,44 @@
 # parse_csv usitility for java project
 import xml.etree.ElementTree as etree
 
-def get_needed_stars(xml_file: str):
-    '''
-    Function will read and parse an xml file
-    '''
-    tree = etree.parse(xml_file)
-    root = tree.getroot()
-    for row in root:
-        for items in row:
-            if items.tag == "Mag":
-                if float(items.text) < 6.0 and float(items.text) > 0.0:
-                    print("StarID: ", row[0].text)
-                    print(items.tag, ":",  items.text)
-                    print("----------------------")
+
+class read_XML:
+
+    def get_needed_stars(self, xml_file: str):
+        '''
+        Function will read and parse an xml file
+        '''
+
+        # Given that these are now accessable,
+        # these items can be used as objects if needed
+        tree = etree.parse(xml_file)
+        root = tree.getroot()
+        for row in root:
+            for items in row:
+                if items.tag == "Mag":
+                    if float(items.text) < 6.0 and float(items.text) > 0.0:
+                        print("StarID: ", row[0].text)
+                        print(items.tag, ":",  items.text)
+                        print("----------------------")
 
 
-def get_planets(xml_file: str):
-    '''
-    Function will read in the planets xml file
-    '''
-    tree = etree.parse(xml_file)
-    root = tree.getroot()
-    for row in root:
-        print("----------------------")
-        for items in row:
-            print(items.tag, items.text)
 
+    def get_planets(self, xml_file: str) -> dict:
+        '''
+        Function will read in the planets xml file
+        '''
+        # Given that these are now accessable,
+        # these items can be used as objects if needed
+        tree = etree.parse(xml_file)
+        root = tree.getroot()
+        planets_dict = {}
+        for row in root:
+            print("-----------------")
+            for items in row:
+               print(items.tag, items.text)
+                
+
+        return planets_dict
 
 
 
@@ -34,10 +46,15 @@ def main():
     '''
     Main Function
     '''
+    # create istance of class
+    rxml = read_XML()
+    # path to both xml files
     path_stars = '\\Users\River\Desktop\Senior_Project\Project\HelperFiles\Resources_for_Students\stars.xml'
     path_planets = '\\Users\River\Desktop\Senior_Project\Project\HelperFiles\Resources_for_Students\planets.xml'
-    get_needed_stars(path_stars)
-    get_planets(path_planets)
+    # use the parents version of method to execute
+    rxml.get_needed_stars(path_stars)
+    planets = rxml.get_planets(path_planets)
+
 
 
 if __name__ == "__main__":
