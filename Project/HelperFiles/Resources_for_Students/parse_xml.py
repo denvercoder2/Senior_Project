a6796179d0f1458d68c6070fb667fa89b274f753
River Holley <denvercoder2@gmail.com>
2019-09-15 14:09:15 -0500

    Added functionality to reader script to read both planet and star xml (Both can now be gathered to be used as objects if needed); Converted planets.csv to an xml for easier working steps

--- Project/HelperFiles/Resources_for_Students/parse_xml.py
+++ Project/HelperFiles/Resources_for_Students/parse_xml.py
@@ -1,7 +1,7 @@
 # parse_csv usitility for java project
 import xml.etree.ElementTree as etree
 
-def read_xml(xml_file: str):
+def get_needed_stars(xml_file: str):
     '''
     Function will read and parse an xml file
     '''
@@ -13,15 +13,31 @@ def read_xml(xml_file: str):
                 if float(items.text) < 6.0 and float(items.text) > 0.0:
                     print("StarID: ", row[0].text)
                     print(items.tag, ":",  items.text)
-                    print("----------------")
+                    print("----------------------")
+
+
+def get_planets(xml_file: str):
+    '''
+    Function will read in the planets xml file
+    '''
+    tree = etree.parse(xml_file)
+    root = tree.getroot()
+    for row in root:
+        print("----------------------")
+        for items in row:
+            print(items.tag, items.text)
+
+
 
 
 def main():
     '''
     Main Function
     '''
-    path = '\\Users\River\Desktop\Senior_Project\Project\HelperFiles\Resources_for_Students\stars.xml'
-    read_xml(path)
+    path_stars = '\\Users\River\Desktop\Senior_Project\Project\HelperFiles\Resources_for_Students\stars.xml'
+    path_planets = '\\Users\River\Desktop\Senior_Project\Project\HelperFiles\Resources_for_Students\planets.xml'
+    get_needed_stars(path_stars)
+    get_planets(path_planets)
 
 
 if __name__ == "__main__":
