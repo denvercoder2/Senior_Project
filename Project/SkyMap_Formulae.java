0f02abbc568e5de09934ef24e951c3fc175a9bc3
River Holley <riverholley0923@gmail.com>
2019-09-18 10:48:52 -0500

    JulianData Function: Type Mismatch error

--- Project/SkyMap_Formulae.java
+++ Project/SkyMap_Formulae.java
@@ -31,20 +31,47 @@ public class SkyMap_Formulae{
         return new_angle;
     }
 
-    // public double getJulianDay(double date, double time){
+    public double JulianDay(int year, int month, int day, int hour, int minute){
         /*
         Function will return the JulianDay
         given the date and time
         */
-         double JulianDay;
-         // whatever java's version of a tuple is
+        int[] date = {year, month, day};
+        int[] time = {hour, minute};
+        double d = day + ((hour + (minute/60)) / 24);
+        int julianDay;
+        int y, m;
+        if (month > 2){
+            y = year;
+            m = month;
+        }
+        else{
+            y = year - 1;
+            m = month + 12;
+        }
+        int[] test_data = {1582, 10, 15};
+        int A, B;
+        // may be error prone depending on how it compares index
+        for (int k = 0; k < date.length; k++){
+            if(date[k] > test_data[k]){
+                A = (int)(y/100); // fractions are dropped
+                B = 2 - A + (int)(A/4);
+
+            }
+            else{
+                B = 0;
+            }
+            julianDay = (int)(365.25 * y) + (int)(30.6001 * (m + 1)) + d + 1720994.5 + B;
+         //  return JulianDay;
+        }
+    }
 
 
-         return JulianDay;
-    // }
     public static void main(String[] args){
         SkyMap_Formulae sky = new SkyMap_Formulae();
         double test_angle = sky.Mod2Pi(365.00);
         System.out.println(test_angle);
+
+
     }
 }
