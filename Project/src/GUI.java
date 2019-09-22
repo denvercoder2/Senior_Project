4b6e2072d38e04e65928e4aadc14d3e9d9f4d93c
Alexx Clark <eec0013@uah.edu>
2019-09-22 14:56:40 -0500

    Created InputUtils class to verify user input.

--- Project/src/GUI.java
+++ Project/src/GUI.java
@@ -11,25 +11,27 @@ import javax.swing.JCheckBox;
 import javax.swing.Box;
 import javax.swing.SwingConstants;
 import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class GUI extends JFrame {
 
-	private JPanel contentPane;
+	private JPanel mainGUI;
 	private final JLabel month = new JLabel("Month");
 	private JTextField monthTextField;
 	private JTextField dayTextField;
 	private JLabel year;
 	private JTextField yearTextField;
-	private JLabel lblTime;
-	private JTextField textField;
-	private JLabel label;
-	private JTextField textField_1;
-	private JLabel lblLatitude;
-	private JTextField textField_2;
-	private JLabel lblLongitude;
-	private JTextField textField_3;
-	private JLabel lblMinutes;
-	private JTextField textField_4;
+	private JLabel time;
+	private JTextField hoursTextField;
+	private JLabel colon;
+	private JTextField minutesTextField;
+	private JLabel latitude;
+	private JTextField latTextField;
+	private JLabel longitude;
+	private JTextField longTextField;
+	private JLabel minutes;
+	private JTextField minTextField;
 
 	/**
 	 * Launch the application.
@@ -53,139 +55,144 @@ public class GUI extends JFrame {
 	public GUI() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 2500, 1500);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		setContentPane(contentPane);
-		contentPane.setLayout(null);
+		mainGUI = new JPanel();
+		mainGUI.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(mainGUI);
+		mainGUI.setLayout(null);
 		month.setFont(new Font("Tahoma", Font.PLAIN, 34));
 		month.setBounds(26, 215, 115, 33);
-		contentPane.add(month);
+		mainGUI.add(month);
 		
 		monthTextField = new JTextField();
 		monthTextField.setFont(new Font("Tahoma", Font.PLAIN, 34));
 		monthTextField.setBounds(142, 213, 52, 36);
-		contentPane.add(monthTextField);
+		mainGUI.add(monthTextField);
 		monthTextField.setColumns(10);
 		
 		JLabel day = new JLabel("Day");
 		day.setFont(new Font("Tahoma", Font.PLAIN, 34));
 		day.setBounds(26, 276, 115, 33);
-		contentPane.add(day);
+		mainGUI.add(day);
 		
 		dayTextField = new JTextField();
 		dayTextField.setFont(new Font("Tahoma", Font.PLAIN, 34));
 		dayTextField.setBounds(142, 276, 52, 39);
-		contentPane.add(dayTextField);
+		mainGUI.add(dayTextField);
 		dayTextField.setColumns(10);
 		
 		year = new JLabel("Year");
 		year.setFont(new Font("Tahoma", Font.PLAIN, 34));
 		year.setBounds(26, 337, 115, 33);
-		contentPane.add(year);
+		mainGUI.add(year);
 		
 		yearTextField = new JTextField();
-		yearTextField.setBounds(142, 343, 52, 39);
-		contentPane.add(yearTextField);
+		yearTextField.setBounds(142, 337, 52, 39);
+		mainGUI.add(yearTextField);
 		yearTextField.setColumns(10);
 		
-		lblTime = new JLabel("Time:");
-		lblTime.setFont(new Font("Tahoma", Font.PLAIN, 34));
-		lblTime.setBounds(26, 399, 115, 33);
-		contentPane.add(lblTime);
+		time = new JLabel("Time:");
+		time.setFont(new Font("Tahoma", Font.PLAIN, 34));
+		time.setBounds(26, 399, 115, 33);
+		mainGUI.add(time);
 		
-		textField = new JTextField();
-		textField.setBounds(26, 441, 49, 39);
-		contentPane.add(textField);
-		textField.setColumns(10);
+		hoursTextField = new JTextField();
+		hoursTextField.setBounds(26, 441, 49, 39);
+		mainGUI.add(hoursTextField);
+		hoursTextField.setColumns(10);
 		
-		label = new JLabel(":");
-		label.setBounds(84, 444, 10, 33);
-		contentPane.add(label);
+		colon = new JLabel(":");
+		colon.setBounds(84, 444, 10, 33);
+		mainGUI.add(colon);
 		
-		textField_1 = new JTextField();
-		textField_1.setBounds(101, 441, 49, 39);
-		contentPane.add(textField_1);
-		textField_1.setColumns(10);
+		minutesTextField = new JTextField();
+		minutesTextField.setBounds(101, 441, 49, 39);
+		mainGUI.add(minutesTextField);
+		minutesTextField.setColumns(10);
 		
-		lblLatitude = new JLabel("Latitude");
-		lblLatitude.setFont(new Font("Tahoma", Font.PLAIN, 34));
-		lblLatitude.setBounds(21, 794, 173, 33);
-		contentPane.add(lblLatitude);
+		latitude = new JLabel("Latitude");
+		latitude.setFont(new Font("Tahoma", Font.PLAIN, 34));
+		latitude.setBounds(21, 794, 173, 33);
+		mainGUI.add(latitude);
 		
-		textField_2 = new JTextField();
-		textField_2.setBounds(186, 794, 52, 39);
-		contentPane.add(textField_2);
-		textField_2.setColumns(10);
+		latTextField = new JTextField();
+		latTextField.setBounds(186, 794, 52, 39);
+		mainGUI.add(latTextField);
+		latTextField.setColumns(10);
 		
-		lblLongitude = new JLabel("Longitude");
-		lblLongitude.setFont(new Font("Tahoma", Font.PLAIN, 34));
-		lblLongitude.setBounds(22, 855, 154, 47);
-		contentPane.add(lblLongitude);
+		longitude = new JLabel("Longitude");
+		longitude.setFont(new Font("Tahoma", Font.PLAIN, 34));
+		longitude.setBounds(22, 855, 154, 47);
+		mainGUI.add(longitude);
 		
-		textField_3 = new JTextField();
-		textField_3.setBounds(186, 862, 52, 39);
-		contentPane.add(textField_3);
-		textField_3.setColumns(10);
+		longTextField = new JTextField();
+		longTextField.setBounds(186, 862, 52, 39);
+		mainGUI.add(longTextField);
+		longTextField.setColumns(10);
 		
-		lblMinutes = new JLabel("Minutes");
-		lblMinutes.setFont(new Font("Tahoma", Font.PLAIN, 34));
-		lblMinutes.setBounds(22, 933, 150, 33);
-		contentPane.add(lblMinutes);
+		minutes = new JLabel("Minutes");
+		minutes.setFont(new Font("Tahoma", Font.PLAIN, 34));
+		minutes.setBounds(22, 933, 150, 33);
+		mainGUI.add(minutes);
 		
-		textField_4 = new JTextField();
-		textField_4.setBounds(186, 933, 52, 39);
-		contentPane.add(textField_4);
-		textField_4.setColumns(10);
+		minTextField = new JTextField();
+		minTextField.setBounds(186, 933, 52, 39);
+		mainGUI.add(minTextField);
+		minTextField.setColumns(10);
 		
-		JCheckBox chckbxStarNames = new JCheckBox("Star Names");
-		chckbxStarNames.setFont(new Font("Tahoma", Font.PLAIN, 34));
-		chckbxStarNames.setBounds(2000, 343, 429, 41);
-		contentPane.add(chckbxStarNames);
+		JCheckBox starNames = new JCheckBox("Star Names");
+		starNames.setFont(new Font("Tahoma", Font.PLAIN, 34));
+		starNames.setBounds(2000, 343, 429, 41);
+		mainGUI.add(starNames);
 		
-		JCheckBox chckbxNewCheckBox = new JCheckBox("Constellations");
-		chckbxNewCheckBox.setFont(new Font("Tahoma", Font.PLAIN, 34));
-		chckbxNewCheckBox.setBounds(2000, 412, 429, 41);
-		contentPane.add(chckbxNewCheckBox);
+		JCheckBox constellations = new JCheckBox("Constellations");
+		constellations.setFont(new Font("Tahoma", Font.PLAIN, 34));
+		constellations.setBounds(2000, 412, 429, 41);
+		mainGUI.add(constellations);
 		
-		JCheckBox chckbxPlanets = new JCheckBox("Planets");
-		chckbxPlanets.setFont(new Font("Tahoma", Font.PLAIN, 34));
-		chckbxPlanets.setBounds(2000, 473, 221, 41);
-		contentPane.add(chckbxPlanets);
+		JCheckBox planets = new JCheckBox("Planets");
+		planets.setFont(new Font("Tahoma", Font.PLAIN, 34));
+		planets.setBounds(2000, 473, 221, 41);
+		mainGUI.add(planets);
 		
-		JCheckBox chckbxMessierDeepSpace = new JCheckBox("Messier Deep Space Objects");
-		chckbxMessierDeepSpace.setFont(new Font("Tahoma", Font.PLAIN, 30));
-		chckbxMessierDeepSpace.setBounds(2000, 534, 429, 41);
-		contentPane.add(chckbxMessierDeepSpace);
+		JCheckBox messierDeepSpace = new JCheckBox("Messier Deep Space Objects");
+		messierDeepSpace.setFont(new Font("Tahoma", Font.PLAIN, 30));
+		messierDeepSpace.setBounds(2000, 534, 429, 41);
+		mainGUI.add(messierDeepSpace);
 		
 		Box verticalBox = Box.createVerticalBox();
 		verticalBox.setBounds(2018, 155, 1, 1);
-		contentPane.add(verticalBox);
+		mainGUI.add(verticalBox);
 		
 		JLabel lblNewLabel = new JLabel("Sky Map");
 		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
 		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 40));
 		lblNewLabel.setBounds(0, 28, 2468, 67);
-		contentPane.add(lblNewLabel);
+		mainGUI.add(lblNewLabel);
 		
-		JButton btnNewButton = new JButton("Apply");
-		btnNewButton.setFont(new Font("Tahoma", Font.BOLD, 34));
-		btnNewButton.setBounds(26, 1082, 212, 67);
-		contentPane.add(btnNewButton);
+		JButton applyButton = new JButton("Apply");
+		applyButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				
+			}
+		});
+		applyButton.setFont(new Font("Tahoma", Font.BOLD, 34));
+		applyButton.setBounds(26, 1082, 212, 67);
+		mainGUI.add(applyButton);
 		
 		JLabel lblEnterLocation = new JLabel("Enter Location:");
 		lblEnterLocation.setFont(new Font("Tahoma", Font.BOLD, 34));
 		lblEnterLocation.setBounds(21, 726, 271, 33);
-		contentPane.add(lblEnterLocation);
+		mainGUI.add(lblEnterLocation);
 		
-		JLabel lblNewLabel_1 = new JLabel("Enter Date:");
-		lblNewLabel_1.setFont(new Font("Tahoma", Font.BOLD, 34));
-		lblNewLabel_1.setBounds(26, 155, 212, 33);
-		contentPane.add(lblNewLabel_1);
+		JLabel enterDate = new JLabel("Enter Date:");
+		enterDate.setFont(new Font("Tahoma", Font.BOLD, 34));
+		enterDate.setBounds(26, 155, 212, 33);
+		mainGUI.add(enterDate);
 		
-		JLabel lblLabels = new JLabel("Labels:");
-		lblLabels.setHorizontalAlignment(SwingConstants.CENTER);
-		lblLabels.setFont(new Font("Tahoma", Font.BOLD, 34));
-		lblLabels.setBounds(2000, 279, 429, 40);
-		contentPane.add(lblLabels);
+		JLabel labels = new JLabel("Labels:");
+		labels.setHorizontalAlignment(SwingConstants.CENTER);
+		labels.setFont(new Font("Tahoma", Font.BOLD, 34));
+		labels.setBounds(2000, 279, 429, 40);
+		mainGUI.add(labels);
 	}
 }
---
+++ Project/src/InputUtils.java
@@ -0,0 +1,51 @@
+
+public class InputUtils {
+
+	private int month;
+	private int day;
+	private int year;
+	private int hours;
+	private int minutes;
+	private int latitude;
+	private int longitude;
+	private int mins;
+	
+	public Boolean canApply()
+	{
+		Boolean applyFlag = false;
+		
+		return applyFlag;
+	}
+	
+	public void setMonth(int input) {
+		month = input;
+	}
+	
+	public void setDay(int input) {
+		day = input;
+	}
+	
+	public void setYear(int input) {
+		year = input;
+	}
+	
+	public void setHours(int input) {
+		hours = input;
+	}
+	
+	public void setMinutes(int input) {
+		minutes = input;
+	}
+	
+	public void setLatitude(int input) {
+		latitude = input;
+	}
+	
+	public void setLongitude(int input) {
+		longitude = input;
+	}
+	
+	public void setMins(int input) {
+		mins = input;
+	}
+}
