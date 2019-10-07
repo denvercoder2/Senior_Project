ae81a43736f26e6f3a9815efbfc9bc977570ce6d
River Holley <denvercoder2@gmail.com>
2019-10-06 22:07:04 -0500

    Daily commit

--- src/ScottWork/Moon.java
+++ src/ScottWork/Moon.java
@@ -1,54 +1,23 @@
 // Placement for the java parsing program
 
 import java.util.*;
+import java.io.*;
+import java.math.*;
 
-public class Moon {
+class Moon {
     // add attributes as needed
-    String phase;
-    boolean visability;
+    String date;
+    String status;
 
     // constructor
-    public Moon(String phase, boolean visability) {
-        this.phase = phase;
-        this.visability = visability;
+    Moon(String date, String status) {
+        this.date = date;
+        this.status = status;
     }
 
-    // if you can see the moon
-    public boolean visible() {
-        return true;
-    }
-
-    /*
-     * Rules include: - Finding the pattern for the phases - Mapping this pattern to
-     * a given date - it takes the moon 29.53 days to complete - A full cycle
-     * through its phases takes 29.53 days - Source:
-     * https://www.subsystems.us/uploads/9/8/9/4/98948044/moonphase.pdf
-     */
-    public static String getPhase(int day, int month, int year) {
-        /*
-         * An array of the phase names to be returned under the certain rules at which
-         * they apply to
-         */
-        String[] phases = { "New Moon", "Waxing Crescent", "First Quater", "Waxing Gibbous", "Full", "Waning Gibbous",
-                "Third Quarter", "Waning Crescent" };
-
-        // iterate the year every 365 iterations
-        // iterate the day every iteration
-        // the cycle takes exactly 29.53 days to go through
-        // completely
-        String current_phase;
-        for (int i = 0; i < phases.length; i++) {
-
-            System.out.println(phases[i]);
-        }
-
-        String sample = "TEST";
-        return sample;
-
-    }
+    
 
     public static void main(String[] args) {
-        String phase = getPhase(10, 16, 1996);
 
     }
 }
--- src/ScottWork/MoonPhase.py
+++ src/ScottWork/MoonPhase.py
@@ -2,6 +2,9 @@
 Moon algorithm in python
 '''
 def moon_phase(month, day, year):
+    '''
+    Moon phase 
+    '''
     ages = [18, 0, 11, 22, 3, 14, 25, 6, 17, 28, 9, 20, 1, 12, 23, 4, 15, 26, 7]
     offsets = [-1, 1, 0, 1, 2, 3, 4, 5, 7, 7, 9, 9]
     description = ["New (totally dark)",
@@ -17,30 +20,36 @@ def moon_phase(month, day, year):
               "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
     if day == 31:
         day = 1
+
     days_into_phase = ((ages[(year + 1) % 19] +
                         ((day + offsets[month-1]) % 30) +
                         (year < 1900)) % 30)
+
     index = int((days_into_phase + 2) * 16/59.0)
     #print(index)  # test
     if index > 7:
         index = 7
+
     status = description[index]
     # light should be 100% 15 days into phase
     light = int(2 * days_into_phase * 100/29)
     if light > 100:
-        light = abs(light - 200);
+        light = abs(light - 200)
     date = "%d%s%d" % (day, months[month-1], year)
+    print(type(date))
+    print(type(status))
+    print(type(light))
     return date, status, light
-# put in a date you want ...
-# 26jan2009 is the start of the Chinese New Year for 2009
-# the moon is at its lowest intensity
 
 def main():
+    '''
+    Main function
+    '''
     month = 10
     day = 5
     year = 2019  # use yyyy format
     date, status, light = moon_phase(month, day, year)
-    print("moon phase on %s is %s, light = %d%s" % (date, status, light, '%'))
+    print("Moon Phase on given date: ", moon_phase(month, day, year))
 
 
 if __name__ == "__main__":
