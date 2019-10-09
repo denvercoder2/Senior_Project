81e735f3bb45913519540a86ebca4e134d4a4390
River Holley <denvercoder2@gmail.com>
2019-10-09 10:32:52 -0500

    More progress on type storage

--- src/ScottWork/Moon.java
+++ src/ScottWork/Moon.java
@@ -7,38 +7,36 @@ import java.math.*;
 
 public class Moon {
     // add attributes as needed
-    String date;
     String status;
     
     // constructor
-    public Moon(){
+    public static class MoonPhase{
+        private String status;    
     }
-    public static void main(String[] args) throws IOException{
-    // int month = 4;
-    // int day = 30;
-    // int year = 1969;
 
-    // ArrayList<String> moon = getPhase(month, day, year);
-    // System.out.println(moon);
-    Runtime rt = Runtime.getRuntime();
-    String[] commands = {"python3.7", "ScottWork/MoonPhase.py"};
-    Process proc = rt.exec(commands);
-    
-    BufferedReader stdInput = new BufferedReader(new 
-         InputStreamReader(proc.getInputStream()));
-    
-    BufferedReader stdError = new BufferedReader(new 
-         InputStreamReader(proc.getErrorStream()));
-    
-    // Read the output from the command
-    String s = null;
-    while ((s = stdInput.readLine()) != null) {
-        System.out.println(s);
+    public static String getPhase() throws IOException{
+        MoonPhase moon = new MoonPhase();
+        Runtime rt = Runtime.getRuntime();
+        String[] commands = {"python3.7", "ScottWork/MoonPhase.py"};
+        Process proc = rt.exec(commands);
+        
+        BufferedReader stdInput = new BufferedReader(new 
+             InputStreamReader(proc.getInputStream()));
+        
+        BufferedReader stdError = new BufferedReader(new 
+             InputStreamReader(proc.getErrorStream()));
+        
+        // Read the output from the command
+        String s = null;
+        while ((s = stdInput.readLine()) != null) {
+            moon.status = s;
+        }
+            return s;
     }
-    
-    // Read any errors from the attempted command
-    while ((s = stdError.readLine()) != null) {
-        System.out.println(s);
+
+    public static void main(String[] args) throws IOException{
+        String phase = getPhase();
+        System.out.println(phase);
         }
     }
-}
+
