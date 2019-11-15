0e40046b0d56357d5b6cffb7b1230e5e6c3c4e8e
River Holley <denvercoder2@gmail.com>
2019-11-15 16:09:23 -0600

    Added RASeconds

--- src/ScottWork/Convert2RA.java
+++ src/ScottWork/Convert2RA.java
@@ -6,51 +6,25 @@ public class Convert2RA{
 
     /*
 *    Function: Convert
-*    Parameters: double, double
+*    Parameters: double, double, double
 *    Return Types: double
 *           Purpose: 
-*    To return the RA from the Hour and Minute           
+*    To return the RA from the Hour, Minute, and Second        
     */
-    public static double Convert(double RAHour, double RAMinute){
-        double RA = (RAHour * 15 + RAMinute * .25);
+    public static double Convert(double RAHour, double RAMinute, double RASeconds){
+        // Ratio for conversion
+        double RA = (RAHour * 15 + RAMinute * .25 + RASeconds * .00416);
 
         return RA;
-    }
-
-    public static void tests(){
-        double RAHour = 20.00;
-        double RAMinute = 12.00;
-        double RA = Convert(RAHour, RAMinute);
-        
-        // java test case 1
-        if(RA != 303.00){
-            System.out.println("Test case Failed");
-        }
-        else{
-            System.out.println("Test case Passed");
-        }
-
-        double RAHour2 = 10.00;
-        double RAMinute2 = 10.00;
-        double RA2 = Convert(RAHour2, RAMinute2);
-
-        if(RA2 != 152.50){
-            System.out.println("Test case Failed");
-            System.out.printf("Answer should be: %.2f", RA2);
-        }
-        else{
-            System.out.println("Test case Passed");
-        }
-
     }
 
     public static void main(String[] args) {
         double RAHour = 20.00;
         double RAMinute = 12.00;
-        tests();
+        double RASecond = 31.00;
 
-        double RA = Convert(RAHour, RAMinute);
+        double RA = Convert(RAHour, RAMinute, RASecond);
 
-        System.out.printf("\nRA Hour and Minute converted to singular RA: %.2f", RA);
+        System.out.printf("\nRA Hour, Minute, and Second converted to singular RA: %.2f", RA);
     }
 }
