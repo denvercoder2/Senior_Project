e3e4c1fa5692cb556335259845964090e37a21a6
River Holley <denvercoder2@gmail.com>
2019-11-15 15:36:50 -0600

    Added some test cases

--- src/ScottWork/Convert2RA.java
+++ src/ScottWork/Convert2RA.java
@@ -17,11 +17,40 @@ public class Convert2RA{
         return RA;
     }
 
+    public static void tests(){
+        double RAHour = 20.00;
+        double RAMinute = 12.00;
+        double RA = Convert(RAHour, RAMinute);
+        
+        // java test case 1
+        if(RA != 303.00){
+            System.out.println("Test case Failed");
+        }
+        else{
+            System.out.println("Test case Passed");
+        }
+
+        double RAHour2 = 10.00;
+        double RAMinute2 = 10.00;
+        double RA2 = Convert(RAHour2, RAMinute2);
+
+        if(RA2 != 152.50){
+            System.out.println("Test case Failed");
+            System.out.printf("Answer should be: %.2f", RA2);
+        }
+        else{
+            System.out.println("Test case Passed");
+        }
+
+    }
+
     public static void main(String[] args) {
         double RAHour = 20.00;
         double RAMinute = 12.00;
+        tests();
 
         double RA = Convert(RAHour, RAMinute);
-        System.out.printf("RA Hour and Minute converted to singular RA: %.2f", RA);
+
+        System.out.printf("\nRA Hour and Minute converted to singular RA: %.2f", RA);
     }
 }
