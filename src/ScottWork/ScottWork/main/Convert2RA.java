accd35a19df5315d3be1f81d80dd790cac3bf3dc
River Holley <denvercoder2@gmail.com>
2019-11-20 09:18:50 -0600

    Added dec formula for John's use (dec degree and minute to one DEC)

--- src/ScottWork/ScottWork/main/Convert2RA.java
+++ src/ScottWork/ScottWork/main/Convert2RA.java
@@ -1,7 +1,7 @@
 /*
  Converting RAHour and Mintute to Singular RA
 */
-package ScottWork.main;
+// package ScottWork;
 public class Convert2RA{
 
     /*
@@ -11,20 +11,41 @@ public class Convert2RA{
 *           Purpose: 
 *    To return the RA from the Hour, Minute, and Second        
     */
-    public static double Convert(double RAHour, double RAMinute){
+    public static double ConvertRA(double RAHour, double RAMinute){
         // Ratio for conversion
         double RA = (RAHour * 15 + RAMinute * .25);
 
         return RA;
     }
+    /*
+*    Function: ConvertDec
+*    Parameters: double, double
+*    Return Types: double
+*           Purpose: 
+*    To return the Dec from Degree and Minute
+    */
+    public static double ConvertDec(double dec, double minute){
+        // Ratio for conversion
+        // 60 archminutes per degree
+        double Dec = (dec + minute/60);
+
+        return Dec;
+    }
+
 
     public static void main(String[] args) {
         double RAHour = 20.00;
         double RAMinute = 12.00;
 
+        double Decdeg = 9.00;
+        double decMin = 24.00;
 
-        double RA = Convert(RAHour, RAMinute);
+        double Dec = ConvertDec(Decdeg, decMin);
+
+
+        double RA = ConvertRA(RAHour, RAMinute);
 
         System.out.printf("\nRA Hour, Minute, and Second converted to singular RA: %.2f", RA);
+        System.out.printf("\nDec Degree and Minute converted to singular Declination: %.2f", Dec);
     }
 }
